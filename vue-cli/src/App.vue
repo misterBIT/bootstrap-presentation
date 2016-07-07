e4f0dcf78b9b7c0548b8b62e082e2a65f0808d19
nadavsinai <ns@nadavsinai.com>
2016-07-07 11:04:07 +0300

    add vue example

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <h1>{{ msg }}</h1>
+        <my-form></my-form>
+    </div>
+</template>
+
+<script>
+    import myForm from './myform.vue';
+    export default {
+        components: {
+            myForm: myForm
+        },
+        data () {
+            return {
+                // note: changing this line won't causes changes
+                // with hot-reload because the reloaded component
+                // preserves its current state and we are modifying
+                // its initial state.
+                msg: 'Hello Vue!'
+            }
+        }
+    }
+</script>
+
+<style>
+    body {
+        font-family: Helvetica, sans-serif;
+    }
+</style>
